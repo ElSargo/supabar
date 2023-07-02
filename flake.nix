364af081cea664775feffa81a2b3e734d4a7b674
Oliver Sargison <sargo@sargo.cc>
2023-07-03 08:15:22 +1200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Minimal rust wasm32-unknown-unknown example";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ rust-overlay.overlay ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        inputs = [ rust pkgs.wasm-bindgen-cli ];
+      in
+      {
+
+
+        devShell = pkgs.mkShell { packages = inputs; };
+      }
+    );
+}
